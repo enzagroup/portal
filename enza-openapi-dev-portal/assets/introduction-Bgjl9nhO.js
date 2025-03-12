934f63e6307e3826723f8fc0b008b3323fca8202
enzagroup <dev@enzagroup.global>
2025-03-12 14:08:11 +0200

    Updates

--- enza-openapi-dev-portal/assets/introduction-Bgjl9nhO.js
+++
@@ -1,7 +0,0 @@
-import{N as i,j as n}from"./entry.client-BqVQEGqy.js";const s="Thanks for choosing enza as your payments partner!",d=[{depth:2,value:"Edit this page",id:"edit-this-page"},{depth:2,value:"Additional Configuration",id:"additional-configuration"}],a={title:"Welcome to enza Open Apis"};function o(t){const e={a:"a",code:"code",h2:"h2",p:"p",...i(),...t.components};return n.jsxs(n.Fragment,{children:[n.jsx(e.p,{children:"Thanks for choosing enza as your payments partner!"}),`
-`,n.jsx(e.h2,{id:"edit-this-page",children:"Edit this page"}),`
-`,n.jsxs(e.p,{children:["To edit this page, open the ",n.jsx(e.code,{inline:!0,children:"introduction.mdx"})," file in the ",n.jsx(e.code,{inline:!0,children:"pages/docs"})," directory."]}),`
-`,n.jsx(e.h2,{id:"additional-configuration",children:"Additional Configuration"}),`
-`,n.jsxs(e.p,{children:["You can find the full configuration for your Zudoku site in the ",n.jsx(e.code,{inline:!0,children:"zudoku.config.ts"})," file."]}),`
-`,n.jsxs(e.p,{children:["For further information on how to customize your site, check out the ",n.jsx(e.a,{href:"https://zudoku.dev/docs",children:"Zudoku documentation"}),"."]})]})}function c(t={}){const{wrapper:e}={...i(),...t.components};return e?n.jsx(e,{...t,children:n.jsx(o,{...t})}):o(t)}export{c as default,s as excerpt,a as frontmatter,d as tableOfContents};
-//# sourceMappingURL=introduction-Bgjl9nhO.js.map
